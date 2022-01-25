fe65d98ec95e0ee2539ab24db489b6e245e889fa
zona-xie123 <37839098+zona-xie123@users.noreply.github.com>
2022-01-25 14:42:33 +0800

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
 	}
 
 	//上傳檔案
-	$(".upload_box").on("change","#upload_input",function(){
+	$(".upload_box").on("input","#upload_input",function(){
 	    var filePath = $(this).val();
 	    var f = this.files[0];
 
